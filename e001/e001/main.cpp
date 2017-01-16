67052ce58155f5f11db7d84b551fe28f18556da0
Johnkim <valentine0310@gmail.com>
2017-01-16 22:55:45 +0900

    중복된 문자열 검출

--- e001/e001/main.cpp
+++ e001/e001/main.cpp
@@ -1,8 +1,31 @@
-#include <iostream>
+﻿#include <iostream>
+#include <map>
 using namespace std;
 
 int main()
 {
-	cout << "Hello World!" << endl;
+	char *str = "1abcdefghijklmn12347";
+	map<char, int> m;
+	int nLen = strlen(str);
+	bool bFind = false;
+
+	for(int i = 0; i<nLen; i++)
+	{
+		map<char, int>::iterator it = m.find(str[i]);
+
+		if (it == m.end())
+			m.insert(pair<char, int>(str[i], i));
+		else
+		{
+			bFind = true;
+			break;
+		}
+	}
+
+	if (bFind)
+		cout << "중복" << endl;
+	else
+		cout << "미중복" << endl;
+
 	return 0;
 }
